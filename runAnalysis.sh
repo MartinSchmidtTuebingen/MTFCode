326d5daef70f213f0768816c3b06a209727cdab9
Martin Schmidt <schmidt.martin@posteo.de>
2022-07-12 13:54:19 +0200

    Small doc updates, check for environment in analysisfolder, dont create analysisFolder if it exists

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
-### ATTENTION: Script has to be executed with source! Otherwise loading the environment does not work as intended -> Check: Really needed? ###
-
 folder=$(pwd)
 
-if [[ $1 = "" ]];then
-  echo "Help not yet implemented"
+if [[ $1 = "" || $1 == "-h" || $1 == "--help" ]];then
+  echo "Help not yet implemented. See README"
 elif [[ $1 = "init" ]];then
   echo "Initialize MTF environment. Password maybe needed to make env.sh executable"
   cp $folder/.env.sh.example $folder/env.sh
   chmod 755 $folder/env.sh
   echo "Done"
 elif [[ $1 = "create" ]];then
-  echo "Create analysis $2"
   analysisName=$2
-  mkdir $folder/$analysisName
-  cp $folder/env.sh $folder/$analysisName/env.sh
-  echo "Created"
+  analysisFolder=$folder/$analysisName
+  if [ -d $sanalysisFolder ];then
+    echo "Analysis already exists"
+    exit
+  fi
+  echo "Create analysis $2"
+  mkdir $analysisFolder
+  cp $folder/env.sh $analysisFolder/env.sh.base
+  echo "Created. Rename env.sh.base in $analysisFolder to env.sh and adjust loading environment, if this analysis should run with another AliPhysics instance."
 else
   analysisName=$1
   analysisFolder=$folder/$analysisName
-  day=$2
-  if [[ $2 = "" ]];then
-    day=$(date +%Y_%m_%d)
+  
+#   day=$2
+#   if [[ $2 = "" ]];then
+#     day=$(date +%Y_%m_%d)
+#   fi
+#   echo "Process systematic from $day"
+
+  if [ -e $analysisFolder/env.sh ];then
+    echo "Source analysis environment"
+    source $analysisFolder/env.sh
+  else
+    echo "Source base environment"
+    source $folder/env.sh
   fi
-  echo "Process systematic from $day"
 
-  source $analysisFolder/env.sh
-
-  python3 $folder/runMTFAnalysis.py $analysisFolder $day
+  python3 $folder/runMTFAnalysis.py $analysisFolder
 fi
